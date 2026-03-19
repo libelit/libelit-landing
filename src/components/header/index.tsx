9f116e9be72d97e0d1ff0a240da56f31eb3212e6
Libelit_Admin <152190999+libelit@users.noreply.github.com>
2026-03-19 13:09:43 +0800

    Update index.tsx restore About Us

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -30,6 +30,37 @@ const Header = async ({ lang = "en" }: { lang: Locale }) => {
           <HomeSidebar nav={translate} lang={lang} />
         </div>
 
+        <nav className="hidden lg:flex items-center text-base justify-between">
+          <div className="p-2 text-zinc-700 flex items-center justify-center">
+            <button className="relative group transition-all duration-200 focus:overflow-visible w-max h-max p-2 overflow-hidden flex flex-row items-center justify-center  btn-link text-lg text-semiBold">
+              <span>{translate.about_us}</span>
+              <svg
+                className="rotate-90 group-focus:rotate-180"
+                xmlns="http://www.w3.org/2000/svg"
+                width="22"
+                height="22"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="m12 10.8l-3.9 3.9q-.275.275-.7.275t-.7-.275q-.275-.275-.275-.7t.275-.7l4.6-4.6q.3-.3.7-.3t.7.3l4.6 4.6q.275.275.275.7t-.275.7q-.275.275-.7.275t-.7-.275z"
+                />
+              </svg>
+              <div className="absolute shadow-lg -bottom-32 left-0 w-full h-max p-2 bg-white border border-zinc-200 rounded-lg flex flex-col gap-2">
+                <span
+                  className="flex flex-row gap-2 items-center hover:bg-zinc-100 p-2 rounded-lg"
+                  onClick={() => window.location.assign(`/${lang}/about`)}
+                >
+                  {translate.team}
+                </span>
+                <WhitepaperButton
+                  text={translate.whitepaper}
+                  isButton={false}
+                  className="flex flex-row gap-2 items-center hover:bg-zinc-100 p-2 rounded-lg"
+                />
+              </div>
+            </button>
+          </div>
           <a
             className="btn-link text-lg text-semiBold"
             href={`/${lang}/roadmap`}
