dbc1878b985dd7314caa05e0176003218e426445
Libelit_Admin <152190999+libelit@users.noreply.github.com>
2026-03-22 19:21:01 +0800

    Update index.tsx FAQ

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -31,7 +31,12 @@ const Header = async ({ lang = "en" }: { lang: Locale }) => {
         </div>
 
         <nav className="hidden lg:flex items-center text-base justify-between">
-          
+
+          </a>
+          <a className="btn-link text-lg text-semiBold" href={`/${lang}/about`}>
+            {translate.faq}
+          </a>
+        
           <a
             className="btn-link text-lg text-semiBold"
             href={`/${lang}`}
