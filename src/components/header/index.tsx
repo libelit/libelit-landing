1c97632fe0eb42ef75bb2706d76c3d68a2ce3636
Libelit_Admin <152190999+libelit@users.noreply.github.com>
2026-03-23 11:38:27 +0800

    Update index.tsx FAQ to about

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -34,7 +34,7 @@ const Header = async ({ lang = "en" }: { lang: Locale }) => {
 
           <a 
             className="btn-link text-lg text-semiBold" 
-            href={`/${lang}/faq`}
+            href={`/${lang}/about`}
             >
             {translate.faq}
           </a>
